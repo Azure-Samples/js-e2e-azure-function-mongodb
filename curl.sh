3ebd175b56c16e783b0a4dbfe89d488f8dcce509
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-09-21 14:41:01 -0700

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,3 +1,5 @@
+# local function API
+
 curl -X POST http://localhost:7071/api/category \
    -H 'Content-Type: application/json' \
    -d '{"document":{"name":"John"}}' --verbose
@@ -14,5 +16,26 @@ curl -X GET http://localhost:7071/api/category \
    -d '{"id":"DOCUMENT_ID"}' --verbose
 
 curl -X DELETE http://localhost:7071/api/category \
+   -H 'Content-Type: application/json' \
+   -d '{"id":"DOCUMENT_ID"}' --verbose
+   
+# remote function API   
+   
+curl -X POST http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?code=YOUR-FUNCTION-KEY \
+   -H 'Content-Type: application/json' \
+   -d '{"document":{"name":"John"}}' --verbose
+
+curl -X POST http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+   -H 'Content-Type: application/json' \
+   -d '{"document":{"name":"Sally"}}' --verbose
+
+curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+   -H 'Content-Type: application/json' --verbose
+
+curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+   -H 'Content-Type: application/json' \
+   -d '{"id":"DOCUMENT_ID"}' --verbose
+
+curl -X DELETE http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
    -H 'Content-Type: application/json' \
    -d '{"id":"DOCUMENT_ID"}' --verbose
