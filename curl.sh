4af71298c90dc45f215b199fb5c7126e85ef9e82
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-09-21 13:45:14 -0700

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,18 +1,18 @@
 curl -X POST http://localhost:7071/api/category \
    -H 'Content-Type: application/json' \
-   -d '{"document":{"name":"John"}}'
+   -d '{"document":{"name":"John"}}' --verbose
 
 curl -X POST http://localhost:7071/api/category \
    -H 'Content-Type: application/json' \
-   -d '{"document":{"name":"Sally"}}'
+   -d '{"document":{"name":"Sally"}}' --verbose
 
 curl -X GET http://localhost:7071/api/category \
-   -H 'Content-Type: application/json' 
+   -H 'Content-Type: application/json' --verbose
 
 curl -X GET http://localhost:7071/api/category \
    -H 'Content-Type: application/json' \
-   -d '{"id":"DOCUMENT_ID"}'
+   -d '{"id":"DOCUMENT_ID"}' --verbose
 
 curl -X DELETE http://localhost:7071/api/category \
    -H 'Content-Type: application/json' \
-   -d '{"id":"DOCUMENT_ID"}'
+   -d '{"id":"DOCUMENT_ID"}' --verbose
