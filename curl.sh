faf529c73327ee76801bdf2ee2886528d12370ce
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-01-05 12:31:01 -0800

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -25,17 +25,17 @@ curl -X POST http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?c
    -H 'Content-Type: application/json' \
    -d '{"document":{"name":"John"}}' --verbose
 
-curl -X POST http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+curl -X POST http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?code=YOUR-FUNCTION-KEY \
    -H 'Content-Type: application/json' \
    -d '{"document":{"name":"Sally"}}' --verbose
 
-curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?code=YOUR-FUNCTION-KEY \
    -H 'Content-Type: application/json' --verbose
 
-curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+curl -X GET http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?code=YOUR-FUNCTION-KEY \
    -H 'Content-Type: application/json' \
    -d '{"id":"DOCUMENT_ID"}' --verbose
 
-curl -X DELETE http://YOUR-FUNCTION-RESOURCE-NAME/api/category?code=YOUR-FUNCTION-KEY \
+curl -X DELETE http://YOUR-FUNCTION-RESOURCE-NAME.azurewebsites.net/api/category?code=YOUR-FUNCTION-KEY \
    -H 'Content-Type: application/json' \
    -d '{"id":"DOCUMENT_ID"}' --verbose
